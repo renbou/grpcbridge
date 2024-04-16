bcbd8b12648acc390d2bdf1de446ffc21f3c15da
Artem Mikheev <30644072+renbou@users.noreply.github.com>
2024-04-16 04:29:07 +0300

    feat: add example embedded usage of grpcbridge to gotestapi

--- examples/gotestapi/go.mod
+++ examples/gotestapi/go.mod
@@ -1,17 +1,20 @@
 module gotestapi
 
-go 1.22
+go 1.22.2
 
 require (
 	github.com/grpc-ecosystem/go-grpc-middleware/v2 v2.0.1
-	google.golang.org/grpc v1.62.0
-	google.golang.org/protobuf v1.32.0
+	github.com/renbou/grpcbridge v0.0.1
+	google.golang.org/grpc v1.63.2
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
-	github.com/golang/protobuf v1.5.3 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.19.1 // indirect
+	golang.org/x/exp v0.0.0-20240409090435-93d18d7e34b8 // indirect
+	golang.org/x/net v0.24.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240412170617-26222e5d3d56 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240412170617-26222e5d3d56 // indirect
 )
--- examples/gotestapi/main.go
+++ examples/gotestapi/main.go
@@ -2,20 +2,27 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
 	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
+	"sync"
 	"syscall"
 	"time"
 
 	gotestapiv1 "gotestapi/gen/proto/gotestapi/v1"
 
 	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
+	"github.com/renbou/grpcbridge"
+	"github.com/renbou/grpcbridge/bridgelog"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/test/bufconn"
 )
 
 func main() {
@@ -41,7 +48,7 @@ func main() {
 		slog.Log(ctx, slog.Level(level), msg, fields...)
 	})
 
-	server := grpc.NewServer(
+	grpcServer := grpc.NewServer(
 		grpc.ChainUnaryInterceptor(logging.UnaryServerInterceptor(gRPCLogger)),
 		grpc.ChainStreamInterceptor(logging.StreamServerInterceptor(gRPCLogger)),
 	)
@@ -50,13 +57,30 @@ func main() {
 	go service.cleaner()
 
 	// Register only v1 gRPC reflection. grpcbridge should properly handle such cases.
-	gotestapiv1.RegisterVeggieShopServiceServer(server, service)
-	reflection.RegisterV1(server)
+	gotestapiv1.RegisterVeggieShopServiceServer(grpcServer, service)
+	reflection.RegisterV1(grpcServer)
+
+	// Embedded grpcbridge support via bufconn.
+	// Currently still requires the reflection server to be present, in the future will support grpc.ServiceRegistrar.
+	bridge := setupBridge(grpcServer)
+
+	// TODO(renbou): use cmux once implemented in grpcbridge.
+	httpServer := &http.Server{
+		Addr:    ":50080",
+		Handler: bridge,
+	}
+
+	go func() {
+		slog.Info("serving test HTTP bridge", "listen_addr", ":50080")
+		if err := httpServer.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			fatalf("failed to serve HTTP server on network", err)
+		}
+	}()
 
 	go func() {
 		slog.Info("serving test gRPC", "listen_addr", listenAddr)
-		if err := server.Serve(lis); err != nil {
-			fatalf("failed to serve", err)
+		if err := grpcServer.Serve(lis); err != nil {
+			fatalf("failed to serve gRPC server on network", err)
 		}
 	}()
 
@@ -68,17 +92,61 @@ func main() {
 
 	// Initiate graceful shutdown in separate goroutine, then,
 	// after a timeout, force the server stop.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*3)
+	defer cancel()
+
 	gracefulCh := make(chan struct{})
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	go func() {
+		defer wg.Done()
+		grpcServer.GracefulStop()
+	}()
+
+	go func() {
+		defer wg.Done()
+		httpServer.Shutdown(ctx)
+	}()
+
+	go func() {
+		wg.Wait()
 		defer close(gracefulCh)
-		server.GracefulStop()
 	}()
 
 	select {
-	case <-time.After(time.Second * 3):
+	case <-ctx.Done():
 	case <-gracefulCh:
 	}
-	server.Stop()
+	grpcServer.Stop()
+}
+
+func setupBridge(server *grpc.Server) *grpcbridge.WebBridge {
+	buf := bufconn.Listen(1 << 20)
+	bridgeLogger := bridgelog.WrapPlainLogger(slog.Default())
+
+	router := grpcbridge.NewReflectionRouter(
+		grpcbridge.WithLogger(bridgeLogger),
+		grpcbridge.WithDialOpts(
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+			grpc.WithContextDialer(func(ctx context.Context, _ string) (net.Conn, error) {
+				return buf.DialContext(ctx)
+			}),
+		),
+		// Disable polling because our protos never change during runtime.
+		grpcbridge.WithDisabledReflectionPolling(),
+	)
+
+	go func() {
+		if err := server.Serve(buf); err != nil {
+			fatalf("failed to serve gRPC server on local bufconn", err)
+		}
+	}()
+
+	router.Add("gotestapi", "passthrough:///bufconn")
+
+	return grpcbridge.NewWebBridge(router, grpcbridge.WithLogger(bridgeLogger))
 }
 
 func fatalf(msg string, err error) {
